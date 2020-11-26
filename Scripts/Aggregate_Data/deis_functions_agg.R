d6535648fe724f8dfd88af25c6b1ac1b99b5b2a3
pmbusch <pmbusch@uc.cl>
2020-11-26 12:27:08 -0300

    Add aggregation of demographic data

--- Scripts/Aggregate_Data/deis_functions_agg.R
+++ Scripts/Aggregate_Data/deis_functions_agg.R
@@ -4,7 +4,7 @@
 
 # Load Data ---------
 source("Scripts/Load_Data/deis_load.R", encoding = "UTF-8")
-source("Scripts/Load_Data/map_load.R", encoding = "UTF-8")
+# source("Scripts/Load_Data/map_load.R", encoding = "UTF-8")
 # source("Scripts/Aggregate_Data/poblacion_agg.R", encoding = "UTF-8")
 theme_set(theme_bw(16)+theme(panel.grid.major = element_blank()))
 
@@ -13,32 +13,6 @@ theme_set(theme_bw(16)+theme(panel.grid.major = element_blank()))
 ## Adjusted MR ----------------
 # MR adjusted to age groups.
 
-## Create age groups
-df_deis$age_group %>% unique()
-# Age group
-df_population$edad %>% unique()
-df_population <- df_population %>% 
-  mutate(age_group=case_when(
-    edad %in% c("0 a 4") ~ "0-4",
-    edad %in% c("5 a 9") ~ "5-9",
-    edad %in% c("10 a 14") ~ "10-14",
-    edad %in% c("15 a 19") ~ "15-19",
-    edad %in% c("20 a 24") ~ "20-24",
-    edad %in% c("25 a 29") ~ "25-29",
-    edad %in% c("30 a 34") ~ "30-34",
-    edad %in% c("35 a 39") ~ "35-39",
-    edad %in% c("40 a 44") ~ "40-44",
-    edad %in% c("45 a 49") ~ "45-49",
-    edad %in% c("50 a 54") ~ "50-54",
-    edad %in% c("55 a 59") ~ "55-59",
-    edad %in% c("60 a 64") ~ "60-64",
-    edad %in% c("65 a 69") ~ "65-69",
-    edad %in% c("70 a 74") ~ "70-74",
-    edad %in% c("75 a 79") ~ "75-79",
-    # edad %in% c("80 a 84") ~ "80-84",
-    T ~ "80+"))
-# df_population %>% group_by(edad, age_group) %>% summarise(count=n()) %>% 
-#   arrange(desc(count)) %>% view()
 
 
 # Function to get adjusted MR rates
--- Scripts/Aggregate_Data/population_agg.R
+++ Scripts/Aggregate_Data/population_agg.R
@@ -5,121 +5,128 @@
 # Load map data with codes and population--------
 source("Scripts/Load_Data/map_load.R", encoding = "UTF-8")
 
-## Dividir en grupos etarios -----------
-df_poblacion$edad %>% unique()
-df_poblacion <- df_poblacion %>% 
-  mutate(grupo_edad=case_when(
-    edad %in% c("0 a 4","5 a 9","10 a 14") ~ "0-14",
-    edad %in% c("15 a 19","20 a 24","25 a 29",
-                "30 a 34","35 a 39","40 a 44") ~ "15-44",
-    edad %in% c("45 a 49","50 a 54","55 a 59","60 a 64") ~ "45-64",
-    edad %in% c("65 a 69", "70 a 74") ~ "65-74",
-    T ~ "75+"))
+# Create Age group
+df_population$edad %>% unique()
+df_population <- df_population %>% 
+  mutate(age_group=case_when(
+    edad %in% c("0 a 4") ~ "0-4",
+    edad %in% c("5 a 9") ~ "5-9",
+    edad %in% c("10 a 14") ~ "10-14",
+    edad %in% c("15 a 19") ~ "15-19",
+    edad %in% c("20 a 24") ~ "20-24",
+    edad %in% c("25 a 29") ~ "25-29",
+    edad %in% c("30 a 34") ~ "30-34",
+    edad %in% c("35 a 39") ~ "35-39",
+    edad %in% c("40 a 44") ~ "40-44",
+    edad %in% c("45 a 49") ~ "45-49",
+    edad %in% c("50 a 54") ~ "50-54",
+    edad %in% c("55 a 59") ~ "55-59",
+    edad %in% c("60 a 64") ~ "60-64",
+    edad %in% c("65 a 69") ~ "65-69",
+    edad %in% c("70 a 74") ~ "70-74",
+    edad %in% c("75 a 79") ~ "75-79",
+    T ~ "80+"))
+# df_population %>% group_by(edad, age_group) %>% summarise(count=n()) %>%
+#   arrange(desc(count)) %>% view()
 
 
-# Para uso en data covid
-df_grupoEdad <- df_poblacion %>% 
-  group_by(codigo_comuna, grupo_edad,sexo) %>% 
-  summarise(poblacion=sum(poblacion,na.rm=T)) %>% ungroup() %>% 
-  mutate(sexo=if_else(sexo %in% c("Mujer","mujer"),"mujer","hombre"))
+# Create major age groups
+df_population <- df_population %>% 
+  mutate(major_age_group=case_when(
+    age_group %in% c("0-4","5-9","10-14") ~ "0-14",
+    age_group %in% c("15-19","20-24","25-29","30-34",
+                     "35-39","40-44") ~ "15-44",
+    age_group %in% c("45-49","50-54","55-59","60-64") ~ "45-64",
+    T ~ "65+"))
+df_population %>% group_by(major_age_group,age_group) %>% summarise(count=n())
 
-# Add 65+ and remove 75+
-df_grupoEdad_65 <- df_grupoEdad %>% 
-  filter(grupo_edad %in% c("65-74", "75+")) %>% 
-  mutate(grupo_edad="65+") %>% 
-  group_by(codigo_comuna, grupo_edad, sexo) %>% 
-  summarise(poblacion=sum(poblacion, na.rm=T))
+## Summarise data as major group age percentages
+df_age <- df_population %>% 
+  mutate(major_age_group=paste("age_",major_age_group,sep="") %>% 
+           str_replace_all("-","_") %>% 
+           str_replace_all("\\+","plus")) %>% 
+  group_by(codigo_comuna, major_age_group) %>% 
+  summarise(pob=sum(poblacion,na.rm=T)) %>% 
+  mutate(perc_age=pob/sum(pob)*100,
+         pob=NULL) %>% ungroup() %>% 
+  # filter(major_age_group!="0-14") %>%
+  spread(major_age_group,perc_age)
 
 # Add 30+
-df_grupoEdad30 <- df_poblacion %>% 
-  filter(edad %in% c("30 a 34","35 a 39","40 a 44",
-                     "45 a 49","50 a 54","55 a 59","60 a 64",
-                     "65 a 69", "70 a 74","75 a 79","80 a 84",
-                     "85 a 89","90 a 94", "95 a 99", "100 o mas")) %>% 
-  mutate(grupo_edad="30+") %>% 
-  group_by(codigo_comuna, grupo_edad, sexo) %>% 
-  summarise(poblacion=sum(poblacion, na.rm=T))
+df_age_30plus <- df_population %>% 
+  mutate(bool_aux=age_group %in% c("30-34","35-39","40-44",
+                                   "45-49","50-54","55-59","60-64",
+                                   "65-69", "70-74","75-79","80+")) %>% 
+  group_by(codigo_comuna,bool_aux) %>% 
+  summarise(poblacion=sum(poblacion, na.rm=T)) %>% 
+  mutate(age_30plus=100*poblacion/sum(poblacion, na.rm=T)) %>% ungroup() %>% 
+  filter(bool_aux==T) %>% select(codigo_comuna, age_30plus)
 
-df_grupoEdad <- df_grupoEdad %>% 
-  filter(!(grupo_edad %in% c("65-74", "75+"))) %>% 
-  rbind(df_grupoEdad_65) %>% 
-  rbind(df_grupoEdad30) %>% 
-  arrange(codigo_comuna, grupo_edad, sexo)
+# Add 75+
+df_age_75plus <- df_population %>% 
+  mutate(bool_aux=age_group %in% c("75-79","80+")) %>% 
+  group_by(codigo_comuna,bool_aux) %>% 
+  summarise(poblacion=sum(poblacion, na.rm=T)) %>% 
+  mutate(age_75plus=100*poblacion/sum(poblacion, na.rm=T)) %>% ungroup() %>% 
+  filter(bool_aux==T) %>% select(codigo_comuna, age_75plus)
 
-df_grupoEdad$poblacion %>% sum()
-rm(df_grupoEdad_65,df_grupoEdad30)
+# Join
+df_age <- df_age %>% 
+  left_join(df_age_30plus, by = c("codigo_comuna")) %>% 
+  left_join(df_age_75plus, by = c("codigo_comuna"))
 
-df_edad <- df_poblacion %>% 
-  group_by(codigo_comuna, grupo_edad) %>% 
-  summarise(pob=sum(poblacion,na.rm=T)) %>% 
-  mutate(perc_edad=pob/sum(pob)*100,
-         pob=NULL) %>% ungroup() %>% 
-  filter(grupo_edad!="0-14") %>% spread(grupo_edad,perc_edad) %>% 
-  mutate(`65+`=`65-74`+`75+`)
 
-# Add 30+
-df_grupoEdad30 <- df_poblacion %>% 
-  mutate(edad30=edad %in% c("30 a 34","35 a 39","40 a 44",
-                     "45 a 49","50 a 54","55 a 59","60 a 64",
-                     "65 a 69", "70 a 74","75 a 79","80 a 84",
-                     "85 a 89","90 a 94", "95 a 99", "100 o mas")) %>% 
-  group_by(codigo_comuna, edad30) %>% 
-  summarise(pob=sum(poblacion,na.rm=T)) %>% 
-  mutate(`30+`=pob/sum(pob)*100,
-         pob=NULL) %>% ungroup() %>% 
-  filter(edad30==T) %>% mutate(edad30=NULL)
-df_edad <- df_edad %>% left_join(df_grupoEdad30, by=c("codigo_comuna"))
-rm(df_grupoEdad30)
 
-## Dividir por sexo
-df_sexo <- df_poblacion %>% 
+## SEX -----------
+df_sex <- df_population %>% 
   group_by(codigo_comuna, sexo) %>% 
   summarise(pob=sum(poblacion,na.rm=T)) %>% 
   mutate(porc=pob/sum(pob)*100,
          pob=NULL) %>% ungroup() %>% 
   filter(sexo!="hombre") %>% spread(sexo,porc) %>%
-  rename(perc_mujer=mujer)
+  rename(perc_female=mujer)
 
 
-## Poblacion por comuna ----------
-df_poblacion <- df_poblacion %>% 
+## Population per commune ----------
+df_population <- df_population %>% 
   group_by(codigo_comuna) %>% 
-  summarise(poblacion=sum(poblacion,na.rm=T))
+  summarise(population=sum(poblacion,na.rm=T)) %>% ungroup()
+df_population$population %>% sum()
 
-# Juntar todo -----
-df_poblacion <- left_join(df_poblacion, df_edad) %>% 
-  left_join(df_sexo) %>% 
+
+# Join all -----
+df_population <- left_join(df_population, df_age) %>% 
+  left_join(df_sex) %>% 
   left_join(codigos_territoriales)
 
-# Limpiar WS
-rm(df_edad,df_sexo)
+# Clean WS
+rm(df_age,df_sex, df_age_30plus, df_age_75plus)
 
 
-## Agregar codigos regionales y datos de superficie ----------
-df_poblacion <- df_poblacion %>% 
-  left_join(mapa_regiones %>% select(codigo_region, region)) %>% 
+## Add area data----------
+df_population <- df_population %>% 
+  left_join(map_region %>% select(codigo_region, region)) %>% 
   select(-geometry) %>% 
-  left_join(mapa_comuna %>% 
+  left_join(map_commune %>% 
               select(codigo_comuna, superficie, perimetro,superficie_censal,
-                     zona,zona_termica))
+                     zone, map_rm))
 
-## Curva acumulada poblacion ------
-# df_poblacion$poblacion %>% sum()
+# # Curva acumulada poblacion ------
 # theme_set(theme_bw(16)+theme(panel.grid.major = element_blank()))
-# df_poblacion %>% 
-#   arrange(desc(poblacion)) %>% 
-#   rowid_to_column() %>% 
-#   ggplot(aes(x=reorder(rowid,desc(poblacion)),y=cumsum(poblacion), group=1))+
+# df_population %>%
+#   arrange(desc(population)) %>%
+#   rowid_to_column() %>%
+#   ggplot(aes(x=reorder(rowid,desc(population)),y=cumsum(population), group=1))+
 #   geom_line(size=1)+
-#   labs(x="N° Comunas", y="Población acumulada")+
+#   labs(x="# Comunas", y="Acc. population")+
 #   scale_y_continuous(labels=function(x) format(x,big.mark = " ", decimal.mark = ".", scientific = F))+
 #   scale_x_discrete(breaks=seq(0,350,50))
 # 
 # source("Scripts/00-Funciones.R", encoding = "UTF-8")
 # f_savePlot(last_plot(),"Figuras/Analisis_general/PobAcumulada.png",dpi=300)
 
-## Densidad segun zonas censales -----------
-# Parametros estadisticos de densidades segun zonas censales
+## Urban density based on disctric zones -----------
+# Some statistics using area from district zones and population
 df_zona <- censo_2017_zonas %>%
   left_join(mapa_zonas) %>% 
   left_join(codigos_territoriales) %>%
@@ -128,20 +135,23 @@ df_zona <- censo_2017_zonas %>%
   na.omit()
 df_zona$poblacion %>% sum()
 
-# Densidad
+# Density
 df_zona <- df_zona %>% 
-  mutate(densidad_pob_manzana=poblacion/superficie*1e6)
+  mutate(urbanDensity=poblacion/superficie*1e6)
 
 ## Agrupar a nivel de comuna
 df_zona <- df_zona %>% 
   group_by(codigo_comuna) %>% 
-  summarise(densidad_pob_manzana_media=mean(densidad_pob_manzana, na.rm=T),
-            densidad_pob_manzana_mediana=median(densidad_pob_manzana, na.rm=T),
-            densidad_pob_manzana_p90=quantile(densidad_pob_manzana,0.9, na.rm=T)) %>% 
+  summarise(urbanDensity_mean=mean(urbanDensity, na.rm=T),
+            urbanDensity_median=median(urbanDensity, na.rm=T),
+            urbanDensity_mean_p90=quantile(urbanDensity,0.9, na.rm=T)) %>% 
   ungroup()
 
 ## Add to df_poblacion
-df_poblacion <- df_poblacion %>% left_join(df_zona)
+df_population <- df_population %>% left_join(df_zona) %>% 
+  mutate(density=population/superficie*1e6,
+         urbanDensity=poblacion/superficie_censal*1e6)
 rm(df_zona)
+
 
 ## EoF
