4f7e5b7214949c0ce61fc317de46a9dc16111d4d
Pablo <pmbusch@uc.cl>
2023-04-26 11:43:39 -0700

    Fig 2 updates

--- Scripts/Figure2.R
+++ Scripts/Figure2.R
@@ -7,12 +7,19 @@
 source("Scripts/00-CargaLibrerias.R", encoding = "UTF-8")
 df <- read.delim("Data/Data_Model/Model_sensitivity.csv",sep=";")
 
+df <- df %>% 
+  mutate(case=str_replace_all(case,"Fixed Radius","Cutoff Distance"),
+         case=str_replace_all(case,"Population commune","Population"),
+         case=str_replace_all(case,"Only Metropolitan region","Metropolitan region only"),
+         case=str_replace_all(case,"10% in each extreme of PM2.5 excluded","PM2.5 within P10-P90"))
+
+
 levels_causes <- c("All Causes (A00-Q99)","Cardiopulmonary (all I and J)",
                    "Cardiovascular (all I)","Pulmonary (all J)")
-levels_case <- c("Base", "Fixed Radius 50km", "Fixed Radius 100km",
-                 "Population commune >50,000", 
-                 "Only Metropolitan region","Without Metropolitan region",
-                 "10% in each extreme of PM2.5 excluded")
+levels_case <- c("Base", "Cutoff Distance 50km", "Cutoff Distance 100km",
+                 "Population >50,000", 
+                 "Metropolitan region only","Without Metropolitan region",
+                 "PM2.5 within P10-P90")
 
 
 # Figure
@@ -31,13 +38,15 @@ df %>%
   # figure
   ggplot(aes(x=fct_rev(case), y=estimate))+
   geom_linerange(aes(ymin=conf.low, ymax=conf.high))+ # bars
-  geom_point(aes(col=sign), size=3.5, alpha=.5)+ #points
-  geom_text(aes(label=est_label),nudge_x = 0.3)+ # LABELhttp://127.0.0.1:35119/graphics/plot_zoom_png?width=1200&height=900
+  geom_point(aes(col=sign), size=3.5)+ #points
+  geom_text(aes(label=est_label),nudge_x = 0.43,nudge_y = 0.03)+ # LABELhttp://127.0.0.1:35119/graphics/plot_zoom_png?width=1200&height=900
   scale_color_manual(values = c("#666666","red"))+guides(col=F)+ # signficant as red
   geom_hline(yintercept = 1, linetype = "dashed")+ # vertical line at 1
   facet_wrap(~cause)+ # by Cause (dependent variable)
-  coord_flip()+
-  scale_y_continuous(labels = function(x) format(x, big.mark = " ",scientific = FALSE))+
+  scale_y_continuous(limits = c(0.83,1.72),
+                     breaks = seq(0.9, 1.4, by = 0.1),
+                     labels = function(x) format(x, big.mark = " ",scientific = FALSE))+
+  coord_flip(ylim = c(0.9,1.4))+ # set limits to exclude long tails
   labs(x="",
        y=expression(paste(
          "MRR: Excess risk per an increase in 10 ","\u03BCg/m\u00B3"," PM2.5"),sep=""), 
@@ -45,6 +54,7 @@ df %>%
   )+
   theme_bw(16)+ 
   theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
         plot.title = element_text(hjust = 0.5),
         plot.caption = element_text(size=10, lineheight=.5))
 
@@ -53,5 +63,8 @@ pdf("Figures/Sensitivity/MMR_summary_nb_label.pdf",
     width = 14.87, height = 9.30)
 last_plot()
 dev.off()
+
+ggsave(file="Figures/Sensitivity/MMR_summary_nb_label.svg", 
+       plot=last_plot(), width=14.87, height=9.30,units = "in")
 
 #EoF
